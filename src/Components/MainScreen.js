53cd7125dce80f035e342950bae5d6db9d6027bc
Nathan Bradshaw <109757621+nathan-bradshaw@users.noreply.github.com>
2024-03-26 16:13:03 -0500

    Added search function

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -4,13 +4,13 @@ import { Container, Row, Col } from 'react-bootstrap';
 import WorkItems from './WorkItems';
 import data from '../Backend_Services/data_op.js'
 import './mainScreen.scss'
-//import {search} from '../Backend_Services/search_data'
+import {search} from '../Backend_Services/search_data'
 
 
 // The main screen with header and search bar
 function MainScreen() {
     const [list,setList] = useState(data)
-    /** 
+    
     function handleChange(e)
     {
         if(e.target.value === ""){
@@ -21,7 +21,7 @@ function MainScreen() {
             setList(res)
         }
     }
-    */
+    
     return (
         <div className='main ' >
         <Container >
@@ -35,7 +35,7 @@ function MainScreen() {
                 <Col md={9}></Col>
                 <Col md = {3}> 
                     <div>
-                    {/*<input placeholder="Search..........."  type="search"  onChange={handleChange}  className='rounded-3 m-1  border-0 w-100 search' />*/}
+                    {<input placeholder="Search..........."  type="search"  onChange={handleChange}  className='rounded-3 m-1  border-0 w-100 search' />}
                     </div>   
 
                 </Col>
