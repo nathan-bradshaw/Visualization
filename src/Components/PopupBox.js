6564435e89337edd8bead35a7b839b755d703c3d
Will T <willtobens@gmail.com>
2024-04-03 14:06:43 -0500

    remade pop up box

--- src/Components/PopupBox.js
+++ src/Components/PopupBox.js
@@ -1,13 +1,24 @@
 import Popup from "reactjs-popup";
-import { Stack, Row,Col, Container } from 'react-bootstrap';
+import { Modal, Button, Row, Col, Container } from 'react-bootstrap';
 import './PopupBox.scss'
-import React from "react";
+import React, {useState} from "react";
 
 // the popup box which pops out on clicking the work
 function PopupBox(props){
+
+    const [showModal, setShowModal] = useState(false);
+
+    const handleOpenModal = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        props.onHide(); // Call onHide prop to handle closing in the parent component
+    };
     
     return (
-        <Popup  className="m-2" trigger={<p> {props.heading}</p>} position=" center center" modal>
+        /** <Popup  className="m-2" trigger={<p> {props.heading}</p>} position=" center center" modal>
             <Container className='popup  m-2 border  rounded-1'>
                 <Row className='m-3'>
                     <Col xs={12}>
@@ -18,9 +29,27 @@ function PopupBox(props){
                     </Col>
                 </Row>
             </Container>
-        </Popup>
+        </Popup> **/
 
+        <>
+            <p onClick={handleOpenModal}>{props.heading}</p>
+            <Modal show={showModal} onHide={handleCloseModal} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>{props.heading}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row className='m-3'>
+                        {props.text}
+                    </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
-  }
+}
   
 export default PopupBox;
